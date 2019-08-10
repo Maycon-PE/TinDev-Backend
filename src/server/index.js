c3d5b465b92fb39dd1e36ee932c94015c6d8a1a0
Maycon Silva <mayconprodigio@hotmail.com>
2019-08-10 15:00:32 -0300

    Real time implemented with socket.io

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const cors = require('cors')
 
+require('../services/database/connection')
+
 const port = process.env.PORT || 3001
 
-const connection = require('../services/database/connection')
-
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const connectedUsers = {
+
+}
+
+io.on('connection', socket => {
+	console.log('ID :', socket.id)
+	const { user } = socket.handshake.query
+
+	connectedUsers[user] = socket.id
+})
+
+app.use((req, res, next) => {
+	req.io = io
+	req.connectedUsers = connectedUsers
+
+	next()
+})
+
 app.use(cors())
-
 app.use(express.json())
-
 require('./routes')(app)
 
-app.listen(port, err => console.log(err? 'Error': 'Running'))
+http.listen(port, err => console.log(err? 'Error': `Running at :${port}` ))
--- src/server/routes/controllers/like.js
+++ src/server/routes/controllers/like.js
@@ -4,13 +4,25 @@ module.exports = {
 	async store(req, res) {
 		const { user } = req.headers
 		const { devId: liked } = req.params
+
 		try {
 			const loggedDev = await Dev.findById(user)
 			const targetDev = await Dev.findById(liked)
 
 			if (!targetDev) return res.status(400).json({ ok: false, error: 'Dev não existe!' })
 
-			if (targetDev.likes.includes(loggedDev._id)) console.log('Deu Math')
+			if (targetDev.likes.includes(loggedDev._id))  {
+				const loggedSocket = req.connectedUsers[user]
+				const targetSocket = req.connectedUsers[liked]
+
+				if (loggedSocket) {
+					req.io.to(loggedSocket).emit('match', targetDev)
+				}
+
+				if (targetSocket) {
+					req.io.to(targetSocket).emit('match', loggedDev)
+				}
+			}
 
 			loggedDev.likes.push(targetDev._id)
 
